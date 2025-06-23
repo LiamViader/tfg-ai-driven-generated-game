b7d75d831aba506f87e06f979bb9c5a02e3097b7
LiamViader <liam.viadr@gmail.com>
2025-06-23 17:50:43 +0200

    asd

--- backend/subsystems/summarize/schemas/graph.state.py
+++ backend/subsystems/summarize/schemas/graph.state.py
@@ -1,17 +1,11 @@
 from typing import Dict, List, Optional, Any, Annotated, Sequence, Literal, Set
 from pydantic import BaseModel, Field
-from copy import deepcopy
-from langchain_core.messages import BaseMessage
-from langgraph.graph.message import add_messages
 import operator
-# El '.' significa "desde el directori actual (schemas)"
-from core_game.map.schemas import ScenarioModel, Direction, OppositeDirections
-from core_game.map.schemas import GameMapModel
 from  subsystems.agents.utils.logs import ToolLog
 
 class SummarizeGraphState(BaseModel):
     operations_to_summarize: Sequence[ToolLog] = Field(default_factory=list, description="list of the operations the sumarize agent has to summarize")
-    sumarized_operations: str = Field(default="", description="Output summary of the operations to summarize")
+    sumarized_operations_result: str = Field(default="", description="Output summary of the operations to summarize")
 
 
     #shared with all other agents
